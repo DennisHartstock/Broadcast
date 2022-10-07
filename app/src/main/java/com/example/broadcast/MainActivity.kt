860ce25ef84d45c00ce37d3666768ddd50524739
DennsHartstock <dennis.hartstock@web.de>
2022-10-07 06:11:58 +0200

    BroadcastReceiver: show loading progress

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.broadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private val receiver = MyReceiver()
+    private lateinit var progressBar: ProgressBar
+
+    private val receiver = object : BroadcastReceiver() {
+
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            if (p1?.action == "loaded") {
+                val percent = p1.getIntExtra("percent", 0)
+                progressBar.progress = percent
+            }
+        }
+    }
+
     private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        progressBar = findViewById(R.id.progressBar)
         findViewById<Button>(R.id.button).setOnClickListener {
             Intent(MyReceiver.ACTION_CLICKED).apply {
                 putExtra(MyReceiver.EXTRA_COUNT, count++)
@@ -26,8 +40,12 @@ class MainActivity : AppCompatActivity() {
             addAction(Intent.ACTION_BATTERY_LOW)
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
             addAction(MyReceiver.ACTION_CLICKED)
+            addAction("loaded")
         }
         registerReceiver(receiver, intentFilter)
+        Intent(this, MyService::class.java).apply {
+            startService(this)
+        }
     }
 
     override fun onDestroy() {
